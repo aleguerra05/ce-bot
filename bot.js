63ee0ab6f491bb4bc9c77f160ad8ca8dc8be1b7c
Alejandro Guerra <aleguerra05@gmail.com>
2021-09-13 13:44:09 -0400

    Add mock server page

--- bot.js
+++ bot.js
@@ -12,3 +12,17 @@ bot.on("text",(msg)=>{
     var chatId = msg.chat.id;
     bot.sendMessage(chatId,`Received: ${msg.text}`);
 });
+
+// Mock server
+const http = require('http');
+const port = 80;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
